8f3e2b3546736185909bb3d33001169f5cab856a
mdamis <mdamis@users.noreply.github.com>
2015-05-23 01:22:03 +0200

    Add movement

--- snake.js
+++ snake.js
@@ -1,3 +1,4 @@
+var ctx;
 var ctxWidth;
 var ctxHeight;
 var gameWidth = 25;
@@ -7,20 +8,60 @@ var cellSize = 20;
 var x = 12;
 var y = 12;
 
+var direction = "down";
+
 window.onload = function() {
 	var canvas = document.getElementById("snake");
 	if(canvas.getContext) {
-		var ctx = canvas.getContext("2d");
+		ctx = canvas.getContext("2d");
 		ctxWidth = canvas.width;
 		ctxHeight = canvas.height;
-		draw(ctx);
+		addEventListener("keydown", keyEvent);
+		setInterval(draw, 100);
 	}
 }
 
-function draw(context) {
-	context.fillStyle = "rgb(0, 0, 0)";
-	context.fillRect(0, 0, ctxWidth, ctxHeight);
+function updatePosition() {
+	switch(direction) {
+		case "left":
+			x = (x - 1 + gameWidth) % gameWidth;
+			break;
+		case "up":
+			y = (y - 1 + gameHeight) % gameHeight;
+			break;
+		case "right":
+			x = (x + 1 + gameWidth) % gameWidth;
+			break;
+		case "down":
+			y = (y + 1 + gameHeight) % gameHeight;
+			break;
+		default:
+	}
+}
 
-	context.fillStyle = 'white';
-	context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+function draw() {
+	updatePosition();
+	ctx.fillStyle = "rgb(0, 0, 0)";
+	ctx.fillRect(0, 0, ctxWidth, ctxHeight);
+
+	ctx.fillStyle = 'white';
+	ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+}
+
+function keyEvent(event) {
+	switch(event.keyCode) {
+		case 37:
+			direction = "left";
+			break;
+		case 38:
+			direction = "up";
+			break;
+		case 39:
+			direction = "right";
+			break;
+		case 40:
+			direction = "down";
+			break;
+		default:
+	}
 }
