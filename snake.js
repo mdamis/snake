970a811a5803560afe61c5e9467c9bdee488e0cf
mdamis <mdamis@users.noreply.github.com>
2015-05-23 12:33:24 +0200

    Add snake

--- snake.js
+++ snake.js
@@ -4,11 +4,23 @@ var ctxHeight;
 var gameWidth = 25;
 var gameHeight = 25;
 var cellSize = 20;
-
-var x = 12;
-var y = 12;
+var snake = [];
 
 var direction = "down";
+
+function Cell(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+function createSnake() {
+	var snake = [];
+
+	for(var i = 0; i < 5; i++) {
+		snake.push(new Cell(12, 12 - i))
+	}
+	return snake;
+}
 
 window.onload = function() {
 	var canvas = document.getElementById("snake");
@@ -16,36 +28,46 @@ window.onload = function() {
 		ctx = canvas.getContext("2d");
 		ctxWidth = canvas.width;
 		ctxHeight = canvas.height;
+		snake = createSnake();
 		addEventListener("keydown", keyEvent);
 		setInterval(draw, 100);
 	}
 }
 
 function updatePosition() {
+	var nextX = snake[0].x;
+	var nextY = snake[0].y;
 	switch(direction) {
 		case "left":
-			x = (x - 1 + gameWidth) % gameWidth;
+			nextX = (nextX - 1 + gameWidth) % gameWidth;
 			break;
 		case "up":
-			y = (y - 1 + gameHeight) % gameHeight;
+			nextY = (nextY - 1 + gameHeight) % gameHeight;
 			break;
 		case "right":
-			x = (x + 1 + gameWidth) % gameWidth;
+			nextX = (nextX + 1 + gameWidth) % gameWidth;
 			break;
 		case "down":
-			y = (y + 1 + gameHeight) % gameHeight;
+			nextY = (nextY + 1 + gameHeight) % gameHeight;
 			break;
 		default:
 	}
+	var tail = snake.pop();
+	tail.x = nextX;
+	tail.y = nextY;
+	snake.unshift(tail);
 }
 
 function draw() {
 	updatePosition();
-	ctx.fillStyle = "rgb(0, 0, 0)";
+	ctx.fillStyle = 'black';
 	ctx.fillRect(0, 0, ctxWidth, ctxHeight);
 
 	ctx.fillStyle = 'white';
-	ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+	for(var i = 0; i < snake.length; i++) {
+		var cell = snake[i];
+		ctx.fillRect(cell.x * cellSize, cell.y * cellSize, cellSize, cellSize);
+	}
 }
 
 function keyEvent(event) {
